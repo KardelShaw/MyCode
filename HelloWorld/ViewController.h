09f013cafc56e63306f2bb4c56dd3a96a0f5f160
xiaoweiguan <a1327793405@gmail.com>
2016-11-14 17:19:17 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Kardel on 16/11/14.
+//  Copyright © 2016年 Kardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Kardel on 16/11/14.
+//  Copyright © 2016年 Kardel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
