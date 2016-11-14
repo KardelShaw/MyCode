0f9eaf855789756c7d9a5f743bfaac5432e39bf2
xiaoweiguan <a1327793405@gmail.com>
2016-11-14 17:31:30 +0800

    hello world
    git test

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"Hello, World");
+    
+    NSLog(@"git test");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
